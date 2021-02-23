c464cd673e38253b557c51766e305dce72609a75
K-Hir-pk18 <74217021+K-Hir-pk18@users.noreply.github.com>
2021-02-23 15:36:08 +0900

    ユーザー管理機能（RSPEC）
    ##WHAT
    ・RSPECの単体テスト
    
    ##WHY
    ・ユーザー管理機能のテストのため

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,84 +1,217 @@
 require 'rails_helper'
-
-RSpec.describe 'ユーザー新規登録', type: :system do
+RSpec.describe 'User', type: :model do
   before do
     @user = FactoryBot.build(:user)
   end
 
+  describe 'ユーザー新規登録', type: :system do
+    context 'ユーザー新規登録ができるとき' do 
+      it '正しい情報を入力すればユーザー新規登録ができてトップページに移動する' do
+        #visit root_path
+        #expect(page).to have_content('新規登録')
+        visit new_user_registration_path
+        @user.valid?
+        #expect{find('input[name= "commit"]').click}.to change { User.count }.by(1)
+        #expect(current_path).to eq root_path
+        #expect(page).to have_content('ログアウト')
+        #expect(page).to have_no_content('新規登録')
+        #expect(page).to have_no_content('ログイン')
+      end
+    end
 
-  context 'ユーザー新規登録ができるとき' do 
-    it '正しい情報を入力すればユーザー新規登録ができてトップページに移動する' do
-      visit root_path
-      expect(page).to have_content('新規登録')
-      visit new_user_registration_path
-      fill_in 'Nickname', with: @user.nickname
-      fill_in 'Email', with: @user.email
-      fill_in 'Password', with: @user.password
-      fill_in 'Password_confirmation', with: @user.password_confirmation
-      fill_in 'family_name_kanji', with: @user.family_name_kanji
-      fill_in 'first_name_kanji', with: @user.first_name_kanji
-      fill_in 'family_name_kana', with: @user.family_name_kana
-      fill_in 'first_name_kana', with: @user.first_name_kana
-      fill_in 'birth_profile', with: @user.birth_profile
+    context 'ユーザー新規登録ができないとき' do
+      it 'ニックネームがないと新規登録できない' do
+        #visit root_path
+        #expect(page).to have_content('新規登録')
+        visit new_user_registration_path
+        @user.nickname = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Nickname can't be blank")
+      end
+    
+      it 'メールアドレスがないと新規登録できない' do
+        #visit root_path
+        #expect(page).to have_content('新規登録')
+        visit new_user_registration_path
+        @user.email = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email can't be blank")
+      end
+    
+      it 'メールアドレスに一意性がないと新規登録できない' do
+        @user.save
+        another_user = FactoryBot.build(:user)
+        another_user.email = @user.email
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include("Email has already been taken")
+      end
+    
+      it 'メールアドレスに＠を含まないと新規登録できない' do
+        #visit root_path
+        #expect(page).to have_content('新規登録')
+        visit new_user_registration_path
+        @user.email = 'aaaaaaaa'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email is invalid")
+      end
+    
+      it 'パスワードがないと新規登録できない' do
+        #visit root_path
+        #expect(page).to have_content('新規登録')
+        visit new_user_registration_path
+        @user.password = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password can't be blank")
+      end
+    
+      it 'パスワードが６文字以上ないと新規登録できない' do
+        #visit root_path
+        #expect(page).to have_content('新規登録')
+        visit new_user_registration_path
+        @user.password = 'aaa22'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+      end
+    
+      it 'パスワードが半角英数字混合でないと新規登録できない(英字のみ)' do
+        #visit root_path
+        #expect(page).to have_content('新規登録')
+        visit new_user_registration_path
+        @user.password = 'aaaaaaa'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password には英字と数字の両方を含めて設定してください")
+      end
+    
+      it 'パスワードが半角英数字混合でないと新規登録できない(数字のみ)' do
+        #visit root_path
+        #expect(page).to have_content('新規登録')
+        visit new_user_registration_path
+        @user.password = '5555555'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password には英字と数字の両方を含めて設定してください")
+      end
+    
+    
+      it 'パスワード確認用がないと新規登録できない' do
+        #visit root_path
+        #expect(page).to have_content('新規登録')
+        visit new_user_registration_path
+        @user.password_confirmation = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+    
+      it 'パスワードとパスワード確認用が一致してないと新規登録できない' do
+        #visit root_path
+        #expect(page).to have_content('新規登録')
+        visit new_user_registration_path
+        @user.password_confirmation = 'aaa333'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
 
-      expect{find('input[name="commit"]').click}.to change { User.count }.by(1)
-      expect(current_path).to eq root_path
+      it 'ユーザー本名は、全角でないと登録できない(名字)' do
+        #visit root_path
+        #expect(page).to have_content('新規登録')
+        visit new_user_registration_path
+        @user.family_name_kanji = 'chikamatsu'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Family name kanji is invalid")
+      end
 
-      expect(find(".user_nav").find("span").hover).to have_content('ログアウト')
-      expect(page).to have_no_content('新規登録')
-      expect(page).to have_no_content('ログイン')
+      it 'ユーザー本名は、全角でないと登録できない(名前)' do
+        #visit root_path
+        #expect(page).to have_content('新規登録')
+        visit new_user_registration_path
+        @user.first_name_kanji = 'mon-zaemon'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name kanji is invalid")
+      end
+
+      it 'ユーザー本名は、全角でないと登録できない(ミョウジ)' do
+        #visit root_path
+        #expect(page).to have_content('新規登録')
+        visit new_user_registration_path
+        @user.family_name_kana = 'chikamatsu'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Family name kana is invalid")
+      end
+
+      it 'ユーザー本名は、全角でないと登録できない(ナマエ)' do
+        #visit root_path
+        #expect(page).to have_content('新規登録')
+        visit new_user_registration_path
+        @user.first_name_kana = 'mon-zaemon'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name kana is invalid")
+      end
+
+
+      it 'ユーザー本名は、名字がないと登録できない' do
+        #visit root_path
+        #expect(page).to have_content('新規登録')
+        visit new_user_registration_path
+        @user.family_name_kanji = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Family name kanji is invalid")
+      end
+
+      it 'ユーザー本名は、名前がないと登録できない' do
+        #visit root_path
+        #expect(page).to have_content('新規登録')
+        visit new_user_registration_path
+        @user.first_name_kanji = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name kanji is invalid")
+      end
+
+      it 'ユーザー本名は、ミョウジがないと登録できない' do
+        #visit root_path
+        #expect(page).to have_content('新規登録')
+        visit new_user_registration_path
+        @user.family_name_kana = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Family name kana is invalid")
+      end
+
+      it 'ユーザー本名は、ナマエがないと登録できない' do
+        #visit root_path
+        #expect(page).to have_content('新規登録')
+        visit new_user_registration_path
+        @user.first_name_kana = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name kana is invalid")
+      end
+      it '生年月日がないと登録できない' do
+        #visit root_path
+        #expect(page).to have_content('新規登録')
+        visit new_user_registration_path
+        @user.birth_profile = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Birth profile can't be blank")
+      end
+
     end
   end
 
-  context 'ユーザー新規登録ができないとき' do
-    it '誤った情報ではユーザー新規登録ができずに新規登録ページへ戻ってくる' do
-      visit root_path
-      expect(page).to have_content('新規登録')
-      visit new_user_registration_path
-      fill_in 'Nickname', with: ""
-      fill_in 'Email', with: ""
-      fill_in 'Password', with: ""
-      fill_in 'Password confirmation', with: ""
-      fill_in 'family_name_kanji', with: ""
-      fill_in 'first_name_kanji', with: ""
-      fill_in 'family_name_kana', with: ""
-      fill_in 'first_name_kana', with: ""
-      fill_in 'birth_profile', with: ""
-      expect{find('input[name="commit"]').click}.to change { User.count }.by(0)
-      expect(current_path).to eq "/users"
+  describe 'ログイン', type: :system do
+    before do
+      @user = FactoryBot.create(:user)
     end
-  end
-end
 
-RSpec.describe 'ログイン', type: :system do
-  before do
-    @user = FactoryBot.create(:user)
-  end
-
-  context 'ログインができるとき' do
-    it '保存されているユーザーの情報と合致すればログインができる' do
-      visit root_path
-      expect(page).to have_content('ログイン')
-      visit new_user_session_path
-      fill_in 'Email', with: @user.email
-      fill_in 'Password', with: @user.password
-      find('input[name="commit"]').click
-      expect(current_path).to eq root_path
-      expect(find(".user_nav").find("span").hover).to have_content('ログアウト')
-      expect(page).to have_no_content('新規登録')
-      expect(page).to have_no_content('ログイン')
-    end
-  end
-
-  context 'ログインができないとき' do
-    it '保存されているユーザーの情報と合致しないとログインができない' do
-      visit root_path
-      expect(page).to have_content('ログイン')
-      visit new_user_session_path
-      fill_in 'Email', with: ""
-      fill_in 'Password', with: ""
-      find('input[name="commit"]').click
-      expect(current_path).to eq new_user_session_path
+    context 'ログインができるとき' do
+      it '保存されているユーザーの情報と合致すればログインができる' do
+        visit root_path
+        expect(page).to have_content('ログイン')
+        visit new_user_session_path
+        fill_in 'user[email]', with: @user.email
+        fill_in 'user[password]', with: @user.password
+        find('input[name="commit"]').click
+        expect(current_path).to eq root_path
+        expect(page).to have_content('ログアウト')
+        expect(page).to have_no_content('新規登録')
+        expect(page).to have_no_content('ログイン')
+      end
     end
   end
 end
