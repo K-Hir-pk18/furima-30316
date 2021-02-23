290f671e8fb0c07da1f4729745f3944f13c345b8
K-Hir-pk18 <74217021+K-Hir-pk18@users.noreply.github.com>
2021-02-23 16:23:19 +0900

    ユーザー管理機能　コメント対応２
    WHAT
    RSPEC単体テストの修正
    
    /spec/models/user_spec.rb
    ```
    visit new_user_registration_path
    ```
    箇所のコメントアウト
    
    WHY
    この記述があると、結合テストになるため

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe 'User', type: :model do
       it '正しい情報を入力すればユーザー新規登録ができてトップページに移動する' do
         #visit root_path
         #expect(page).to have_content('新規登録')
-        visit new_user_registration_path
+        #visit new_user_registration_path
         @user.valid?
         #expect{find('input[name= "commit"]').click}.to change { User.count }.by(1)
         #expect(current_path).to eq root_path
@@ -23,7 +23,7 @@ RSpec.describe 'User', type: :model do
       it 'ニックネームがないと新規登録できない' do
         #visit root_path
         #expect(page).to have_content('新規登録')
-        visit new_user_registration_path
+        #visit new_user_registration_path
         @user.nickname = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Nickname can't be blank")
@@ -32,7 +32,7 @@ RSpec.describe 'User', type: :model do
       it 'メールアドレスがないと新規登録できない' do
         #visit root_path
         #expect(page).to have_content('新規登録')
-        visit new_user_registration_path
+        #visit new_user_registration_path
         @user.email = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Email can't be blank")
@@ -49,7 +49,7 @@ RSpec.describe 'User', type: :model do
       it 'メールアドレスに＠を含まないと新規登録できない' do
         #visit root_path
         #expect(page).to have_content('新規登録')
-        visit new_user_registration_path
+        #visit new_user_registration_path
         @user.email = 'aaaaaaaa'
         @user.valid?
         expect(@user.errors.full_messages).to include("Email is invalid")
@@ -58,7 +58,7 @@ RSpec.describe 'User', type: :model do
       it 'パスワードがないと新規登録できない' do
         #visit root_path
         #expect(page).to have_content('新規登録')
-        visit new_user_registration_path
+        #visit new_user_registration_path
         @user.password = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Password can't be blank")
@@ -67,7 +67,7 @@ RSpec.describe 'User', type: :model do
       it 'パスワードが６文字以上ないと新規登録できない' do
         #visit root_path
         #expect(page).to have_content('新規登録')
-        visit new_user_registration_path
+        #visit new_user_registration_path
         @user.password = 'aaa22'
         @user.valid?
         expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
@@ -76,7 +76,7 @@ RSpec.describe 'User', type: :model do
       it 'パスワードが半角英数字混合でないと新規登録できない(英字のみ)' do
         #visit root_path
         #expect(page).to have_content('新規登録')
-        visit new_user_registration_path
+        #visit new_user_registration_path
         @user.password = 'aaaaaaa'
         @user.valid?
         expect(@user.errors.full_messages).to include("Password には英字と数字の両方を含めて設定してください")
@@ -85,7 +85,7 @@ RSpec.describe 'User', type: :model do
       it 'パスワードが半角英数字混合でないと新規登録できない(数字のみ)' do
         #visit root_path
         #expect(page).to have_content('新規登録')
-        visit new_user_registration_path
+        #visit new_user_registration_path
         @user.password = '5555555'
         @user.valid?
         expect(@user.errors.full_messages).to include("Password には英字と数字の両方を含めて設定してください")
@@ -95,7 +95,7 @@ RSpec.describe 'User', type: :model do
       it 'パスワード確認用がないと新規登録できない' do
         #visit root_path
         #expect(page).to have_content('新規登録')
-        visit new_user_registration_path
+        #visit new_user_registration_path
         @user.password_confirmation = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
@@ -104,7 +104,7 @@ RSpec.describe 'User', type: :model do
       it 'パスワードとパスワード確認用が一致してないと新規登録できない' do
         #visit root_path
         #expect(page).to have_content('新規登録')
-        visit new_user_registration_path
+        #visit new_user_registration_path
         @user.password_confirmation = 'aaa333'
         @user.valid?
         expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
@@ -113,7 +113,7 @@ RSpec.describe 'User', type: :model do
       it 'ユーザー本名は、全角でないと登録できない(名字)' do
         #visit root_path
         #expect(page).to have_content('新規登録')
-        visit new_user_registration_path
+        #visit new_user_registration_path
         @user.family_name_kanji = 'chikamatsu'
         @user.valid?
         expect(@user.errors.full_messages).to include("Family name kanji is invalid")
@@ -122,7 +122,7 @@ RSpec.describe 'User', type: :model do
       it 'ユーザー本名は、全角でないと登録できない(名前)' do
         #visit root_path
         #expect(page).to have_content('新規登録')
-        visit new_user_registration_path
+        #visit new_user_registration_path
         @user.first_name_kanji = 'mon-zaemon'
         @user.valid?
         expect(@user.errors.full_messages).to include("First name kanji is invalid")
@@ -131,7 +131,7 @@ RSpec.describe 'User', type: :model do
       it 'ユーザー本名は、全角でないと登録できない(ミョウジ)' do
         #visit root_path
         #expect(page).to have_content('新規登録')
-        visit new_user_registration_path
+        #visit new_user_registration_path
         @user.family_name_kana = 'chikamatsu'
         @user.valid?
         expect(@user.errors.full_messages).to include("Family name kana is invalid")
@@ -140,7 +140,7 @@ RSpec.describe 'User', type: :model do
       it 'ユーザー本名は、全角でないと登録できない(ナマエ)' do
         #visit root_path
         #expect(page).to have_content('新規登録')
-        visit new_user_registration_path
+        #visit new_user_registration_path
         @user.first_name_kana = 'mon-zaemon'
         @user.valid?
         expect(@user.errors.full_messages).to include("First name kana is invalid")
@@ -150,7 +150,7 @@ RSpec.describe 'User', type: :model do
       it 'ユーザー本名は、名字がないと登録できない' do
         #visit root_path
         #expect(page).to have_content('新規登録')
-        visit new_user_registration_path
+        #visit new_user_registration_path
         @user.family_name_kanji = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Family name kanji is invalid")
@@ -159,7 +159,7 @@ RSpec.describe 'User', type: :model do
       it 'ユーザー本名は、名前がないと登録できない' do
         #visit root_path
         #expect(page).to have_content('新規登録')
-        visit new_user_registration_path
+        #visit new_user_registration_path
         @user.first_name_kanji = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("First name kanji is invalid")
@@ -168,7 +168,7 @@ RSpec.describe 'User', type: :model do
       it 'ユーザー本名は、ミョウジがないと登録できない' do
         #visit root_path
         #expect(page).to have_content('新規登録')
-        visit new_user_registration_path
+        #visit new_user_registration_path
         @user.family_name_kana = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Family name kana is invalid")
@@ -177,7 +177,7 @@ RSpec.describe 'User', type: :model do
       it 'ユーザー本名は、ナマエがないと登録できない' do
         #visit root_path
         #expect(page).to have_content('新規登録')
-        visit new_user_registration_path
+        #visit new_user_registration_path
         @user.first_name_kana = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("First name kana is invalid")
@@ -185,7 +185,7 @@ RSpec.describe 'User', type: :model do
       it '生年月日がないと登録できない' do
         #visit root_path
         #expect(page).to have_content('新規登録')
-        visit new_user_registration_path
+        #visit new_user_registration_path
         @user.birth_profile = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Birth profile can't be blank")
