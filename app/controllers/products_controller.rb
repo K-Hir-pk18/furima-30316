4fd726405c1785ed7fc77487f9ee77186ccc21cf
K-Hir-pk18 <74217021+K-Hir-pk18@users.noreply.github.com>
2021-03-06 16:21:04 +0900

    レビュー対応３
    WHAT
    ・product_controllerの改良
    
    ```
      def create
        @product = Product.new(product_params)
        if @product.save
          redirect_to root_path
        else
          render :new
        end
      end
    ```
    
    WHY
    ・レビュー対応
    
    ・良いコードにするため
    
    ・優秀なエンジニアに近づくため！

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -13,8 +13,7 @@ class ProductsController < ApplicationController
 
   def create
     @product = Product.new(product_params)
-    @product.save
-        if @product.valid?
+    if @product.save
       redirect_to root_path
     else
       render :new
